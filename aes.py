33279803753532150a1816c7ff31bad8f41157e6
Mona Lisa <nikruzhok@gmail.com>
2020-08-08 21:36:47 +0300

    segment_size=128

--- aes.py
+++ aes.py
@@ -1,4 +1,3 @@
-import json
 import base64
 import hashlib
 from Cryptodome import Random
@@ -8,34 +7,17 @@ from Cryptodome.Cipher import AES
 class AESCipher(object):
 
     def __init__(self, key):
-        self.bs = AES.block_size
-        key_bytes = base64.b64decode(key)
-        self.key = hashlib.sha256(key_bytes).digest()
-        print(key, self.key.hex(), AES.block_size)
+        self.key = key
 
     def encrypt(self, raw):
-        # raw = 'world'
         iv = Random.new().read(AES.block_size)
-        # iv = b'0123456789ABCDEF'
-        cipher = AES.new(self.key, AES.MODE_CFB, iv)
-        print(000, raw.encode())
+        cipher = AES.new(self.key, AES.MODE_CFB, iv, segment_size=128)
         encrypted = cipher.encrypt(raw.encode())
-        print(111, json.dumps(base64.b64encode(iv).decode()))
-        print(222, json.dumps(base64.b64encode(encrypted).decode()))
         return base64.b64encode(iv + encrypted).decode()
 
     def decrypt(self, enc):
         enc = base64.b64decode(enc.encode())
         iv = enc[:AES.block_size]
         body = enc[AES.block_size:]
-        print(333, json.dumps(base64.b64encode(iv).decode()))
-        print(444, json.dumps(base64.b64encode(body).decode()))
-        cipher = AES.new(self.key, AES.MODE_CFB, iv)
+        cipher = AES.new(self.key, AES.MODE_CFB, iv, segment_size=128)
         return cipher.decrypt(body).decode()
-
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
-
-    @staticmethod
-    def _unpad(s):
-        return s[:-ord(s[len(s) - 1:])]
--- index.py
+++ index.py
@@ -1,4 +1,6 @@
-import logr
+from logr import Logr
 
+priv = 'MC0CAQACBQDIOyKzAgMBAAECBQCHaZwRAgMA0nkCAwDziwIDAL+xAgJMKwICGq0='
+logr = Logr(priv)
 
 logr.log("info", "hello")
